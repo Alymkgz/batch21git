abc01a3c91dda2e6d6d7cad1b584bca34c944337
Alymkgz <azanbaevalym@gmail.com>
2024-06-12 18:45:59 -0700

    stshing message

--- src/main/java/Alym.java
+++ src/main/java/Alym.java
@@ -2,5 +2,6 @@ public class Alym {
     public static void main(String[] args) {
         System.out.println("1 message from git ");
         System.out.println("git add after add ");
+        System.out.println("Message from stash");
     }
 }
